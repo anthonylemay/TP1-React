dfa31760dc36b37b3a1a2382c2579383ed3625e3
anthonylemay <anthony_lemay@hotmail.com>
2023-06-28 03:49:24 -0400

    clean up code (commentaires enlevés)

--- Multiverse-Pokedex/src/components/CardTitle/CardTitle.jsx
+++ Multiverse-Pokedex/src/components/CardTitle/CardTitle.jsx
@@ -3,6 +3,6 @@ import "./CardTitle.css";
 
 const CardTitle = (props) => {
   return <h2 className="card__title">{props.title}</h2>;
-}; // ne pas oublier que si c'est actif, mettre en h1
+};
 
 export default CardTitle;
